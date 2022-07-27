3c922d6e7a71c174c4382e7fa30dfdf3408086d5
Rajat <guptraj15@gmail.com>
2022-07-27 14:49:01 +0530

    added

--- js introduction/function.js
+++ js introduction/function.js
@@ -1,5 +1,5 @@
 
-function multiplay(a,b)
+function multiplay(a,b)   // function is keyword
 {
     console.log(a*b)
 }
@@ -18,4 +18,21 @@ function subs(a,b)
     console.log(a-b)
 }
 
-subs(15,10)
+subs(15, 10)
+
+
+
+// function as first class citizens
+
+let sayHi = function()
+{
+    console.log('hi')
+}
+
+sayHi()
+// IIFE(immediately invoked function expression)
+
+let add = (function (a, b)
+{return a+b})(10, 20)
+
+console.log(add)
